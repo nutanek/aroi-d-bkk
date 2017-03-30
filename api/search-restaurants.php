5ad4095575388e62b7833bafda3030cb3bb93765
Praphawit <nero.addict@hotmail.com>
2017-03-31 00:30:27 +0700

    finish search restaurant Second Page

--- api/search-restaurants.php
+++ api/search-restaurants.php
@@ -7,23 +7,59 @@
 		$obj = json_decode($json);
 		$data = (array) $obj;
 
+		if ($_GET['type'] == 1) {
+			if (!isset($data['areaRes']) ||
+				!isset($data['keyword']) ) {
+				echo json_encode(array(
+					"status" => "error"
+				));
+				exit(0);
+			}
 
-		if (!isset($data['areaRes']) ||
-			!isset($data['keyword']) ) {
-			echo json_encode(array(
-				"status" => "error"
-			));
-			exit(0);
-		}
+
+			$sql = "select *
+			        from   restaurant
+			        where  (content like '%$data[keyword]%'
+			            or  type_res = '$data[keyword]'
+			            or  name_res like '%$data[keyword]%')
+			            and area_res = '$data[areaRes]'";
+        } else if ($_GET['type'] == 2) {
+        	if (!isset($data['areaRes']) ||
+				!isset($data['keyword']) ||
+				!isset($data['minPrice']) ||
+				!isset($data['maxPrice']) ) {
+				echo json_encode(array(
+					"status" => "error"
+				));
+				exit(0);
+			}
+
+			$sqlQuery = "select * from restaurant ";
 
 
-		$sql = "select *
-		        from   restaurant
-		        where  (content like '%$data[keyword]%'
-		            or type_res = '$data[keyword]'
-		            or name_res = '$data[keyword]')
-		            and area_res = '$data[areaRes]'";
-                    
+			if ($data[keyword] != null) {
+				$sqlAppend[0] = "(name_res like '%$data[keyword]%' or type_res = '$data[keyword]' or content like '%$data[keyword]%')"; 
+			}
+			if ($data[areaRes] != null) {
+				$sqlAppend[1] = "area_res = '$data[areaRes]'";
+			}
+			if ($data[minPrice] != null && $data[maxPrice] != null) {
+				$sqlAppend[2] = "min_price >= '$data[minPrice]' and max_price <= '$data[maxPrice]'";
+			}
+
+			$sqlAppendAll = implode(" and ", $sqlAppend);
+			if (sizeof($sqlAppend) > 0) {
+				$sql = $sqlQuery." where ".$sqlAppendAll;
+			} else {
+				$sql = $sqlQuery.$sqlAppendAll;
+			}
+			
+        }
+
+
+
+
+
 
 		if ($result = $conn->query($sql)) {
 			$header = array("status" => "success");
