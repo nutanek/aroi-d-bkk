5f1f1fa35bd06baa61a6b9224de138422e99eac0
Praphawit <nero.addict@hotmail.com>
2017-03-30 22:57:00 +0700

    finish search restaurants HOMEPAGE

--- api/search-restaurants.php
+++ api/search-restaurants.php
@@ -8,43 +8,43 @@
 		$data = (array) $obj;
 
 
-		if (isset($data['areaRes']) &&
-			isset($data['keyword']) ) {
+		if (!isset($data['areaRes']) ||
+			!isset($data['keyword']) ) {
+			echo json_encode(array(
+				"status" => "error"
+			));
+			exit(0);
+		}
 
 
-			$sql = "select *
-		            from   restaurant
-		            where  content like '%$data[keyword]%'
-		            	or type_res = '$data[keyword]'
-
-		            	
-		            	";
+		$sql = "select *
+		        from   restaurant
+		        where  (content like '%$data[keyword]%'
+		            or type_res = '$data[keyword]'
+		            or name_res = '$data[keyword]')
+		            and area_res = '$data[areaRes]'";
                     
 
-		   if ($result = $conn->query($sql)) {
-				$header = array("status" => "success");
-				$body = array();
-					while ($row = $result->fetch_object()) {
-		        		array_push($body, array(
-							"id" => $row->id_res,
-							"name" => $row->name_res,
-							"type" => $row->type_res,
-							"area" => $row->area_res,
-							"price" => array(
-								"min" => $row->min_price,
-								"max" => $row->max_price
-							),
-							"img" => $row->img,
-							"content" => $row->content
-						));
-		   			 }
-				$restaurants = array_merge($header, array("body" => $body));
-				echo json_encode($restaurants, JSON_UNESCAPED_UNICODE);
-			} else {
-				echo json_encode(array(
-					"status" => "error1"
+		if ($result = $conn->query($sql)) {
+			$header = array("status" => "success");
+			$body = array();
+			while ($row = $result->fetch_object()) {
+		        array_push($body, array(
+					"id" => $row->id_res,
+					"name" => $row->name_res,
+					"type" => $row->type_res,
+					"area" => $row->area_res,
+					"price" => array(
+						"min" => $row->min_price,
+						"max" => $row->max_price
+					),
+					"img" => $row->img,
+					"content" => $row->content
 				));
-			}
+		   	}
+			$restaurants = array_merge($header, array("body" => $body));
+			echo json_encode($restaurants, JSON_UNESCAPED_UNICODE);
+	
 		} else {
 			echo json_encode(array(
 				"status" => "error"
